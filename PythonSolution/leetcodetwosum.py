d7162502751138c42e5581bf5f0f61b71bbf47f0
FWKT-Zhang3 <max.zhang2020@outlook.com>
2020-09-03 13:56:10 +1000

    debug python Two Sum

--- PythonSolution/leetcodetwosum.py
+++ PythonSolution/leetcodetwosum.py
@@ -1,6 +1,12 @@
-class TwoSum(object):
+class Solution:
     def twoSum(self, nums, target):
-        for i, num in enumrate(nums):
+        for i,num in enumerate(nums):
             for j in range(i+1, len(nums)):
-                if num + nums[j] == target:
+                if num+nums[j] == target:
                     return [i, j]
+
+if __name__ == "__main__":
+    a = Solution()
+    b = a.twoSum([2,3,3], 5)
+    for i in b:
+        print(i)
