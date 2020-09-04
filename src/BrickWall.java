558763305fe37e15aa1d919420bf88425f043c47
FWKT-Zhang3 <max.zhang2020@outlook.com>
2020-09-05 00:24:41 +1000

    Finish question - Brick Wall & Reverse Integer

---
+++ src/BrickWall.java
@@ -0,0 +1,65 @@
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * Created by Max Zhang on 2020/9/4.
+ */
+public class BrickWall {
+
+    /*
+    * 自己写的算法，有些笨重
+    * 不过表现还可以
+    *
+    * Time Submitted    | Status    | Runtime   | Memory    | Language
+    * 09/04/2020 22:50	Accepted	12 ms	    42.7 MB	    java
+    *
+    */
+    public int leastBricks(List<List<Integer>> wall) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int temp;
+        for (List<Integer> integers : wall) {
+            temp = 0;
+            for (Integer integer : integers) {
+                temp += integer;
+                if (map.containsKey(temp)) {
+                    map.put(temp, map.get(temp) + 1);
+                } else {
+                    map.put(temp, 1);
+                }
+            }
+        }
+        Object[] a = map.values().toArray();
+        Arrays.sort(a);
+        if (a.length == 1) {
+            return wall.size();
+        }
+        return wall.size() - (int)a[a.length-2];
+
+
+//        改良之后，效果好了一些
+//        ######################################################################
+//        09/04/2020 22:57	Accepted	10 ms	42.2 MB	java                   #
+//        ######################################################################
+//        HashMap<Integer, Integer> map = new HashMap<>();
+//        int temp;
+//        for (List<Integer> integers : wall) {
+//            temp = 0;
+//            for (int i = 0; i < integers.size() - 1; i++) {
+//                temp += integers.get(i);
+//                if (map.containsKey(temp)) {
+//                    map.put(temp, map.get(temp) + 1);
+//                } else {
+//                    map.put(temp, 1);
+//                }
+//            }
+//        }
+//
+//        if (map.keySet().size() == 0) {
+//            return wall.size();
+//        }
+//        Object[] a = map.values().toArray();
+//        Arrays.sort(a);
+//        return wall.size() - (int)a[a.length-1];
+    }
+}
---
+++ src/ReverseInteger.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Max Zhang on 2020/9/5.
+ *
+ * 嫖的大佬的想法，思维僵化啊
+ * 不能总想着遍历
+ *
+ * 09/05/2020 00:19	Accepted	1 ms	37.1 MB	java
+ */
+public class ReverseInteger {
+    public int reverse(int x) {
+        int result = 0;
+        int newResult;
+        while (x != 0) {
+            int tail = x % 10;
+            newResult = result * 10 + tail;
+            if ((newResult- tail) / 10 != result) return 0;
+            result = newResult;
+            x /= 10;
+        }
+        return result;
+    }
+}
