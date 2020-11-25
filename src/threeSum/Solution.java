aa3f1176dfcc5253e7e8f44d49698b241c3a47a6
FWKT-Zhang3 <max.zhang2020@outlook.com>
2020-11-25 18:37:44 +1000

    3Sum & 3Sum Closest

---
+++ src/threeSum/Solution.java
@@ -0,0 +1,50 @@
+package threeSum;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @Author: Max Zhang
+ * @Date: 2020/11/25 14:45
+ * @Description: Given an array nums of n integers, are there elements a, b, c
+ *      in nums such that a + b + c = 0? Find all unique triplets in the array
+ *      which gives the sum of zero.
+ *
+ * Notice that the solution set must not contain duplicate triplets.
+ */
+public class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int n = nums.length;
+        if (n < 3) return ans;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+
+            int left = i + 1, right = n - 1, sum;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0) {
+                    ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                } else if (sum < 0) left++;
+                else right--;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        long start = System.currentTimeMillis();
+        Solution s = new Solution();
+        System.out.println(s.threeSum(new int[]{-1,0,1,2,-1,-4}));
+        long endTime = System.currentTimeMillis();
+        System.out.println("运行时间： " + (endTime - start) + "ms");
+    }
+}
---
+++ src/threeSumClosest/Solution.java
@@ -0,0 +1,67 @@
+package threeSumClosest;
+
+import java.util.Arrays;
+
+/**
+ * @Author: Max Zhang
+ * @Date: 2020/11/25 17:07
+ * @Description: Given an array nums of n integers and an integer target, find
+ *      three integers in nums such that the sum is closest to target. Return
+ *      the sum of the three integers. You may assume that each input would have
+ *      exactly one solution.
+ */
+public class Solution {
+    public int threeSumClosest(int[] nums, int target) {
+
+        // 可读性很强，也很简洁
+        int n = nums.length, minValue = Integer.MAX_VALUE;
+
+        Arrays.sort(nums);
+
+        for (int i = 0; i < n - 2; i++) {
+            if (i > 0 && nums[i] == nums[i-1]) continue;
+            int left = i + 1, right = n - 1, sum;
+            while (left < right) {
+                sum = nums[i] + nums[left] + nums[right] - target;
+                if (sum == 0) {
+                    return target;
+                }
+                if (Math.abs(sum) < Math.abs(minValue)) minValue = sum;
+                if (sum > 0) right--;
+                else left++;
+            }
+        }
+
+        return minValue + target;
+
+//        int n = nums.length, minValue = Integer.MAX_VALUE, ans = 0;
+//
+//        Arrays.sort(nums);
+//
+//        for (int i = 0; i < n - 2; i++) {
+//            if (i > 0 && nums[i] == nums[i-1]) continue;
+//
+//            int left = i + 1, right = n - 1, sum, r;
+//            while (left < right) {
+//                sum = nums[i] + nums[left] + nums[right];
+//                r = sum - target;
+//                if (r == 0) {
+//                    return sum;
+//                } else if (r < 0) {
+//                    if (Math.abs(r) < minValue) {
+//                        minValue = Math.abs(r);
+//                        ans = sum;
+//                    }
+//                    left++;
+//                } else {
+//                    if (Math.abs(r) < minValue) {
+//                        minValue = Math.abs(r);
+//                        ans = sum;
+//                    }
+//                    right--;
+//                }
+//            }
+//        }
+//        return ans;
+    }
+}
