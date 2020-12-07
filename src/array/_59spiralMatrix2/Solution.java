e71407d3ed0ea81970d3ae7b26a4029730750279
FWKT-Zhang3 <max.zhang2020@outlook.com>
2020-12-07 14:43:37 +1000

    Spiral Matrix 2

--- src/array/_59spiralMatrix2/Solution.java
+++ src/array/_59spiralMatrix2/Solution.java
@@ -1,5 +1,7 @@
 package array._59spiralMatrix2;
 
+import java.util.Arrays;
+
 /**
  * @Author: Max Zhang
  * @Date: 2020/12/4 18:21
@@ -8,6 +10,42 @@ package array._59spiralMatrix2;
  */
 public class Solution {
     public int[][] generateMatrix(int n) {
-        return null;
+        if (n == 1) return new int[][]{{1}};
+        int[][] ans = new int[n][n];
+        int rStart = 0, cStart = 0, rEnd = n - 1, cEnd = n - 1, temp = 1;
+        while (rStart <= rEnd && cStart <= cEnd) {
+            for (int i = cStart; i <= cEnd; i++) {
+                ans[rStart][i] = temp++;
+            }
+            rStart++;
+
+            for (int i = rStart; i <= rEnd; i++) {
+                ans[i][cEnd] = temp++;
+            }
+            cEnd--;
+
+            if (rStart < rEnd) {
+                for (int i = cEnd; i >= cStart; i--) {
+                    ans[rEnd][i] = temp++;
+                }
+                rEnd--;
+            }
+
+            if (cStart <= cEnd) {
+                for (int i = rEnd; i >= rStart; i--) {
+                    ans[i][cStart] = temp++;
+                }
+                cStart++;
+            }
+        }
+        return ans;
+    }
+
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        int[][] c = s.generateMatrix(5);
+        for (int[] a : c) {
+            System.out.println(Arrays.toString(a));
+        }
     }
 }
